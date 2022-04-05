e8d676b5842371979ed8de37922f1fea75cde9b6
Masesesam <101641781+Masesesam@users.noreply.github.com>
2022-04-05 15:26:47 +0300

    Add files via upload

---
+++ _.c product of two numbers .c
@@ -0,0 +1,13 @@
+/*a program to calculate area of a circle*/
+/*28-4-2022*/
+/*Sam masese*/
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("please enter any two numbers\n");
+scanf("%d,%d",&a,&b);
+c=a*b;
+printf("the product of two numbers%d",c);
+return 0;
+}
