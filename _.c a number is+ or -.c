aadae6cb99dcdbacf2c116576a2a780f640a84c1
Masesesam <101641781+Masesesam@users.noreply.github.com>
2022-04-05 15:27:30 +0300

    Add files via upload

---
+++ _.c a number is+ or -.c
@@ -0,0 +1,13 @@
+/*a program to determine whether a number is positive or negative */
+/*1-4-2022*/
+/*Samwel masese*/
+#include<stdio.h>
+int main()
+{
+int r=34;
+
+printf("%c");
+scanf("to identify whether a number is positive or negative");
+return 0;
+
+}
