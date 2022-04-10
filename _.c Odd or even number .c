1ec5bbc92e4566dfcacdbd38804c3d1c807cbc52
Masesesam <101641781+Masesesam@users.noreply.github.com>
2022-04-10 12:08:23 +0300

    Add files via upload

---
+++ _.c Odd or even number .c
@@ -0,0 +1,25 @@
+//a program to identify whether a number is odd or even
+//Samwel masese
+//5-4-2022
+#include<stdio.h>
+int main()
+
+{int num;
+printf("enter an integer");
+scanf("%d",&num);
+/*the number must completely divisible by 2*/;
+
+if(num%=2)
+
+
+{printf("the number is odd",num);
+
+   scanf("%d",&num);}
+else
+
+{
+   printf("the number is even",&num);
+scanf("%d",&num);
+}
+return 0;
+}
