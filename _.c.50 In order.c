fca3a4016becc22bfaa21e309b4558a121f387ad
Masesesam <101641781+Masesesam@users.noreply.github.com>
2022-04-19 14:31:52 +0300

    Add files via upload

---
+++ _.c.50 In order.c
@@ -0,0 +1,19 @@
+/*a program to write the 50 numbers in reverse*/
+/*Samwel Masese*/
+/*15-4-2022*/
+
+#include<stdio.h>
+
+
+void main()
+{
+int i;
+printf("The first 50 numbers using a for loop\n");
+for(i=1;i<=50;++i)//for(initialize;condition;increment/decrement)
+{
+printf("%d\n",i);
+}
+//loop
+}
+
+  
