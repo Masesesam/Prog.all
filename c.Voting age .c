6788c7164fefd708ec57d6e93016f1b1b693f0fe
Masesesam <101641781+Masesesam@users.noreply.github.com>
2022-04-26 09:38:37 +0300

    Add files via upload

---
+++ c.Voting age .c
@@ -0,0 +1,23 @@
+/*elligible age to vote when you are a kenyan citizen*/
+/*Samwel Masese*/
+/*22-4-2022*/
+
+#include<stdio.h>
+void main()
+{
+
+int a;
+
+printf("is a kenyan citizen\n");
+printf("enter age\n");
+
+scanf("%d",&a);
+if(a>=18)
+printf("can vote");
+else
+
+
+printf("can not vote");
+
+
+}
